cc5bd636134b400d5126c7c4a796e0129b0ad40b
sawedth <sametrefikd@gmail.com>
2025-03-23 16:22:13 +0300

    UpdateV2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
 import './App.css'
-import {Switch, Route, useHistory} from 'react-router-dom';
+import { Switch, Route, useHistory } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './components/Main';
 import Order from './components/Order';
 import OrderConf from './components/OrderConf';
+import Footer from './components/Footer';
 
 function App() {
-  
 
   return (
     <>
       <div className='content-selector'>
-        <h1>HELLO</h1>
-        {/*
-                <Switch>
-                <Route exact path="/">
-                  <Main />
-                </Route>
-                <Route exact path="/order">
-                  <Order />
-                </Route>
-                <Route exact path="/orderconf">
-                  <OrderConf />
-                </Route>
-              </Switch>
-              */
-
-        }
-
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route exact path="/order">
+            <Order />
+          </Route>
+          <Route exact path="/orderconf">
+            <OrderConf />
+          </Route>
+        </Switch>
       </div>
-      <Footer />
     </>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,31 @@
-import {Button } from 'reactstrap';
-export default function Main(){
+import { Button } from 'reactstrap';
+import banner from '../assets/home-banner.png'
+import logo from '../assets/logo.svg'
+import { useHistory } from 'react-router-dom';
+
+const background = {
+    width: "100%",
+    height: "100vh",
+    backgroundImage: "url(" + banner + ")"
+};
+
+const Banner = styled.div`
+display: flex;
+flex-direction: column;
+`;
+
+export default function Main() {
+    let history = useHistory();
+    function handleClick() {
+        history.push("/order");
+    }
+
     return (
-    <>
-        <Button />
-    </>);
+        <div style={background}>
+            <div className='banner' >
+                <img src={logo} />
+                <Button color="warning" onClick={handleClick}>ACIKTIM</Button>
+            </div>
+
+        </div>);
 };
