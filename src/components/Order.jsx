81aa5819d057650e54c24b0714372530ca21c2d7
sawedth <sametrefikd@gmail.com>
2025-03-27 12:44:35 +0300

    IT2-end-wout-tests

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,9 +1,10 @@
 import logo from '../assets/logo.svg'
 import './Order.css';
-import { Form, FormGroup, Input, Label, Col, Button, ButtonGroup, Card, CardBody, CardTitle, CardText } from 'reactstrap';
+import { Form, FormGroup, Input, Label, Col, Button, Card, CardBody, CardTitle, CardText } from 'reactstrap';
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Footer from './Footer.jsx'
+import formBanner from '../assets/pictures/form-banner.png'
 
 const mockData = {
     name: "",
@@ -14,8 +15,10 @@ const mockData = {
     note: ""
 }
 
+const tick = {}
+
 export default function Order(prop) {
-    const {handleSubmit} = prop;
+    const { handleSubmit } = prop;
     const [size, setSize] = useState("");
     const [pizzaCounter, setPizzaCounter] = useState(1)
     const [toppings, setToppings] = useState([]);
@@ -37,8 +40,13 @@ export default function Order(prop) {
 
     }
 
+    function handleSizeClick(event) {
+        event.preventDefault();
+        setSize(event.target.innerText);
+    }
+
     useEffect(() => {
-        setPizzaValue(pizzaCounter*(toppings.length * 5 + 85.50));
+        setPizzaValue(pizzaCounter * (toppings.length * 5 + 85.50));
         setUser({
             name: name,
             size: size,
@@ -47,18 +55,19 @@ export default function Order(prop) {
             toppings: toppings,
             note: orderNote
         })
-        
-        if(size !== "" && toppings.length < 11 && toppings.length > 3 && name.length > 3){
+
+        if (size !== "" && toppings.length < 11 && toppings.length > 3 && name.length > 3) {
             setIsDisabled(false);
-        }else{
+        } else {
             setIsDisabled(true);
         }
     }, [size, pizzaCounter, toppings, selectedDough, name, orderNote])
 
     return (
-        <>
-            <div className="header">
+        <div className='order-main'>
+            <div className="order-header">
                 <img src={logo} alt="Teknolojik Yemekler" id='header-logo' />
+                <img src={formBanner} alt="Position Absolute Acı Pizza" id='header-banner' />
                 <nav className='header-nav'>
                     <a href="/">Anasayfa</a>
                     <a href="/">Seçenekler</a>
@@ -82,36 +91,32 @@ export default function Order(prop) {
 
                     <Col className='col-1'>
                         <div className='size-radio'>
-                            <Label for="button" id='me-2-form'><b>Boyut Seç</b></Label>
-                            <ButtonGroup onClick={(event) => event.preventDefault()}>
-                                <Button
+                            {<Label for="button" id='me-2-form'><b>Boyut Seç</b></Label>}
+                            <div className='size-buttons'>
+                                <button
+                                    className={"size-button " + (size === "S" ? "active-button" : "passive")}
                                     name='s'
-                                    color="primary"
-                                    outline
-                                    onClick={() => setSize("S")}
-                                    active={size === "S"}
+                                    onClick={handleSizeClick}
                                 >
                                     S
-                                </Button>
-                                <Button
+                                </button>
+                                <button
+                                    className={"size-button " + (size === "M" ? "active-button" : "passive")}
                                     name='m'
-                                    color="primary"
-                                    outline
-                                    onClick={() => setSize("M")}
-                                    active={size === "M"}
+                                    type="checkbox"
+                                    onClick={handleSizeClick}
                                 >
                                     M
-                                </Button>
-                                <Button
-                                    name='l'
-                                    color="primary"
-                                    outline
-                                    onClick={() => setSize("L")}
-                                    active={size === "L"}
+                                </button>
+                                <button
+                                    className={"size-button " + (size === "L" ? "active-button" : "passive")}
+                                    name='m'
+                                    onClick={handleSizeClick}
                                 >
                                     L
-                                </Button>
-                            </ButtonGroup>
+                                </button>
+                            </div>
+
                         </div>
                         <div className='dough-select'>
                             <Label
@@ -153,170 +158,62 @@ export default function Order(prop) {
                     <p>Lütfen en az 4, en fazla 10 malzeme seçiniz.</p>
                 </div>
                 <div className='checkboxes'>
-
-
-                    <div className='checkbox-column' >
-                        <FormGroup
-                            check
-                            inline
-
-                            onClick={handleCheckbox}
-                        >
-                            <Input type="checkbox" name="Peperoni" />
-                            <Label check>
-                                Peperoni
-                            </Label>
-                        </FormGroup>
-                        <FormGroup
-                            check
-                            inline
-                            onClick={handleCheckbox}
-                        >
-                            <Input type="checkbox" name="Sosis" />
-                            <Label check>
-                                Sosis
-                            </Label>
-                        </FormGroup>
-                        <FormGroup
-                            check
-                            inline
-
-                            onClick={handleCheckbox}
-                        >
-                            <Input type="checkbox" name="Kanada Jambonu" />
-                            <Label check>
-                                Kanada Jambonu
-                            </Label>
-                        </FormGroup>
-                        <FormGroup
-                            check
-                            inline
-
-                            onClick={handleCheckbox}
-                        >
-                            <Input type="checkbox" name="Tavuk Izgara" />
-                            <Label check>
-                                Tavuk Izgara
-                            </Label>
-                        </FormGroup>
-                        <FormGroup
-                            check
-                            inline
-
-                            onClick={handleCheckbox}
-                        >
-                            <Input type="checkbox" name="Soğan" />
-                            <Label check>
-                                Soğan
-                            </Label>
-                        </FormGroup>
-                    </div>
-                    <div className='checkbox-column'><FormGroup
-                        check
-                        inline
-
-                        onClick={handleCheckbox}
-                    >
-                        <Input type="checkbox" name="Domates" />
-                        <Label check>
-                            Domates
-                        </Label>
-                    </FormGroup>
-                        <FormGroup
-                            check
-                            inline
-
-                            onClick={handleCheckbox}
-                        >
-                            <Input type="checkbox" name="Mısır" />
-                            <Label check>
-                                Mısır
-                            </Label>
-                        </FormGroup>
-                        <FormGroup
-                            check
-                            inline
-
-                            onClick={handleCheckbox}
-                        >
-                            <Input type="checkbox" name="Sucuk" />
-                            <Label check>
-                                Sucuk
-                            </Label>
-                        </FormGroup>
-                        <FormGroup
-                            check
-                            inline
-
-                            onClick={handleCheckbox}
-                        >
-                            <Input type="checkbox" name="Jalepano" />
-                            <Label check>
-                                Jalepano
-                            </Label>
-                        </FormGroup>
-                        <FormGroup
-                            check
-                            inline
-
-                            onClick={handleCheckbox}
-                        >
-                            <Input type="checkbox" name="Sarımsak" />
-                            <Label check>
-                                Sarımsak
-                            </Label>
-                        </FormGroup>
-                    </div>
-                    <div className='checkbox-column last-checkbox'>
-                        <FormGroup
-                            check
-                            inline
-                            className='last-checkbox'
-
-                            onClick={handleCheckbox}
-                        >
-                            <Input type="checkbox" name="Biber" />
-                            <Label check>
-                                Biber
-                            </Label>
-                        </FormGroup>
-                        <FormGroup
-                            check
-                            inline
-                            className='last-checkbox'
-
-                            onClick={handleCheckbox}
-                        >
-                            <Input type="checkbox" name="Sucuk" />
-                            <Label check>
-                                Sucuk
-                            </Label>
-                        </FormGroup>
-                        <FormGroup
-                            check
-                            inline
-                            className='last-checkbox'
-
-                            onClick={handleCheckbox}
-                        >
-                            <Input type="checkbox" name="Ananas" />
-                            <Label check>
-                                Ananas
-                            </Label>
-                        </FormGroup>
-                        <FormGroup
-                            check
-                            inline
-                            className='last-checkbox'
-
-                            onClick={handleCheckbox}
-                        >
-                            <Input type="checkbox" name="Kabak" />
-                            <Label check>
-                                Kabak
-                            </Label>
-                        </FormGroup>
-                    </div>
+                    <label className="checkbox-container">Peperoni
+                        <input type="checkbox" name="Peperoni"onClick={handleCheckbox}/>
+                            <span class="checkmark"></span>
+                    </label>
+                    <label className="checkbox-container">Sosis
+                        <input type="checkbox" name="Sosis" onClick={handleCheckbox}/>
+                            <span class="checkmark"></span>
+                    </label>
+                    <label className="checkbox-container">Kanada Jambonu
+                        <input type="checkbox" name="Kanada Jambonu" onClick={handleCheckbox}/>
+                            <span class="checkmark"></span>
+                    </label>
+                    <label className="checkbox-container">Tavuk Izgara
+                        <input type="checkbox" name="Tavuk Izgara" onClick={handleCheckbox}/>
+                            <span class="checkmark"></span>
+                    </label>
+                    <label className="checkbox-container">Soğan
+                        <input type="checkbox" name="Soğan"onClick={handleCheckbox}/>
+                            <span class="checkmark"></span>
+                    </label>
+                    <label className="checkbox-container">Domates
+                        <input type="checkbox" name="Domates" onClick={handleCheckbox}/>
+                            <span class="checkmark"></span>
+                    </label>
+                    <label className="checkbox-container">Mısır
+                        <input type="checkbox" name="Mısır" onClick={handleCheckbox}/>
+                            <span class="checkmark"></span>
+                    </label>
+                    <label className="checkbox-container">Sucuk
+                        <input type="checkbox" name="Sucuk" onClick={handleCheckbox}/>
+                            <span class="checkmark"></span>
+                    </label>
+                    <label className="checkbox-container">Jalepeno
+                        <input type="checkbox" name="Jalepeno"onClick={handleCheckbox}/>
+                            <span class="checkmark"></span>
+                    </label>
+                    <label className="checkbox-container">Sarımsak
+                        <input type="checkbox" name="Sarımsak" onClick={handleCheckbox}/>
+                            <span class="checkmark"></span>
+                    </label>
+                    <label className="checkbox-container">Biber
+                        <input type="checkbox" name="Biber" onClick={handleCheckbox}/>
+                            <span class="checkmark"></span>
+                    </label>
+                    <label className="checkbox-container">Salam
+                        <input type="checkbox" name="Salam" onClick={handleCheckbox}/>
+                            <span class="checkmark"></span>
+                    </label>
+                    <label className="checkbox-container">Ananas
+                        <input type="checkbox" name="Ananas" onClick={handleCheckbox}/>
+                            <span class="checkmark"></span>
+                    </label>
+                    <label className="checkbox-container">Kabak
+                        <input type="checkbox" name="Kabak" onClick={handleCheckbox}/>
+                            <span class="checkmark"></span>
+                    </label>
                 </div>
                 <div className='notes'>
                     <Label
@@ -324,7 +221,6 @@ export default function Order(prop) {
                     >
                         <b>Ad ve Soyad</b>
                     </Label>
-
                     <Input
                         id="name"
                         name="name"
@@ -356,7 +252,7 @@ export default function Order(prop) {
                             onClick={(event) => {
                                 event.preventDefault();
                                 pizzaCounter === 1 ? setPizzaCounter(1) : setPizzaCounter(pizzaCounter - 1);
-                                
+
                             }}>-</button>
                         <p className="pizza-count">{pizzaCounter}</p>
                         <button className="pizza-button"
@@ -373,11 +269,11 @@ export default function Order(prop) {
                     >
                         <CardBody className='pizza-card-body'>
                             <div className="pizza-card-text" >
-                                <CardTitle tag="h5" style={{color:"black"}}>
+                                <CardTitle tag="h5" style={{ color: "black" }}>
                                     Sipariş Toplamı
                                 </CardTitle>
 
-                                <div className='order-lines' style={{color:"black"}}>
+                                <div className='order-lines' style={{ color: "black" }}>
                                     <CardText>
                                         Seçimler:
                                     </CardText>
@@ -386,14 +282,14 @@ export default function Order(prop) {
                                     </CardText>
                                 </div>
                                 <div className='order-lines'>
-                                    <CardText style={{color:"red"}}>
+                                    <CardText style={{ color: "red" }}>
                                         Toplam:
                                     </CardText>
-                                    <CardText style={{color:"red"}}>
-                                    {
-                                    
-                                    `${pizzaValue.toFixed(2)}₺`
-                                    }
+                                    <CardText style={{ color: "red" }}>
+                                        {
+
+                                            `${pizzaValue.toFixed(2)}₺`
+                                        }
                                     </CardText>
                                 </div>
 
@@ -409,6 +305,6 @@ export default function Order(prop) {
 
             </Form>
             <Footer />
-        </>
+        </div>
     );
 };
